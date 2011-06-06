0b12a63fe09b36fcd997bddab125a0a772db3c0f
Joce <jocelynlegault@gmail.com>
2011-06-05 21:30:45 -0600

    Solution to problem 67.
    Signed-off-by: Joce <jocelynlegault@gmail.com>

---
+++ Problem067.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public static class Problem067
+    {
+        // The results I got are of the following order of magnitude:
+        // 
+        // Problem 67, Solution 1: Value = 7273 in 8025 ticks
+
+        static Stopwatch _timer = new Stopwatch();
+
+        [EulerSolution]
+        // Fast solution, same as Problem 18's fast solution. Not sure how it can be sped up at the moment.
+        public static void Solution1()
+        {
+            _timer.Restart();
+            var triangle = File.ReadAllLines("Problem067.data").Select(line => line.Split(' ').Select(item => int.Parse(item)).ToArray()).ToArray();
+
+            int[][] temp = new int[2][];
+            temp[0] = new int[triangle[triangle.Length-1].Length];
+            temp[1] = new int[triangle[triangle.Length-1].Length];
+
+            int cur = 0;
+            int next = 1;
+
+            temp[cur][0] = triangle[0][0];
+            for (int i = 1; i < triangle.Length; i++)
+            {
+                for (int j = 0; j < triangle[i-1].Length; j++)
+                {
+                    int v1 = temp[cur][j] + triangle[i][j];
+                    int v2 = temp[cur][j] + triangle[i][j+1];
+
+                    if (temp[next][j] < v1)
+                    {
+                        temp[next][j] = v1;
+                    }
+
+                    if (temp[next][j+1] < v2)
+                    {
+                        temp[next][j+1] = v2;
+                    }
+                }
+
+                temp[cur].Initialize();
+
+                cur ^= 1;
+                next ^= 1;
+            }
+            _timer.Stop();
+
+            int result = temp[cur].Max();
+
+            Trace.WriteLine(string.Format("Problem 67, Solution 1: Value = {0} in {1} ticks", result, _timer.ElapsedTicks));
+        }
+    }
+}
