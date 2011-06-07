fa611dfa4da459c762c4e2c1997c9dadfee34800
Joce <jocelynlegault@gmail.com>
2011-06-06 21:29:23 -0600

    Clean up the main program type loop.
    Signed-off-by: Joce <jocelynlegault@gmail.com>

--- Program.cs
+++ Program.cs
@@ -16,29 +16,28 @@ namespace EulerProject
 
             foreach (var t in Assembly.GetExecutingAssembly().GetTypes()
                                                              .Where(t => t.IsPublic && 
-                                                                         t.GetCustomAttributes(typeof(EulerProblemAttribute), false).Any())
+                                                                         t.GetCustomAttributes(typeof(EulerProblemAttribute), false).Any() &&
+                                                                         t.Name.StartsWith("Problem") &&
+                                                                         problems.Contains(t.Name.Substring("Problem".Length).TrimStart('0')))
                                                              .OrderBy(t => t.Name))
             {
-                if (t.Name.StartsWith("Problem") && problems.Contains(t.Name.Substring("Problem".Length).TrimStart('0')))
+                foreach (var m in t.GetMethods()
+                                   .Where(m => m.IsStatic && 
+                                               m.IsPublic && 
+                                               m.GetCustomAttributes(typeof(EulerSolutionAttribute), false).Any())
+                                   .OrderBy(m => m.Name))
                 {
-                    foreach (var m in t.GetMethods()
-                                       .Where(m => m.IsStatic && 
-                                                   m.IsPublic && 
-                                                   m.GetCustomAttributes(typeof(EulerSolutionAttribute), false).Any())
-                                       .OrderBy(m => m.Name))
+                    var attrib = ((EulerSolutionAttribute)m.GetCustomAttributes(typeof(EulerSolutionAttribute), false).First());
+                    if (attrib.IsEnabled)
                     {
-                        var attrib = ((EulerSolutionAttribute)m.GetCustomAttributes(typeof(EulerSolutionAttribute), false).First());
-                        if (attrib.IsEnabled)
-                        {
-                            m.Invoke(null, null);
-                        }
-                        else
-                        {
-                            Trace.WriteLine(string.Format("Problem {0}, Solution {1}: Disabled ({2})", 
-                                                           t.Name.Substring("Problem".Length).TrimStart('0'),
-                                                           m.Name.Substring("Solution".Length),
-                                                           attrib.Reason));
-                        }
+                        m.Invoke(null, null);
+                    }
+                    else
+                    {
+                        Trace.WriteLine(string.Format("Problem {0}, Solution {1}: Disabled ({2})", 
+                                                        t.Name.Substring("Problem".Length).TrimStart('0'),
+                                                        m.Name.Substring("Solution".Length),
+                                                        attrib.Reason));
                     }
                 }
             }
