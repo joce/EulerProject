fd0782c4317c9ba77c68eb18393ac7c1b123accc
joce <jocelynlegault@gmail.com>
2016-01-30 11:33:33 -0500

    Add problem template.
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ Problem.template.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem000
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = xxx in xxx ticks
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            timer.Stop();
+            return 0;
+        }
+    }
+}
