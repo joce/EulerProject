9f0713ed9cc163cc3317704ac72e400132627b49
joce <jocelynlegault@gmail.com>
2016-01-29 19:50:37 -0500

    New solution to problem 23.
    An order of magnitude faster!
    
    Signed-off-by: joce <jocelynlegault@gmail.com>

--- Problem023.cs
+++ Problem023.cs
@@ -14,6 +14,7 @@ namespace EulerProject
         // Problem 23, Solution 2: Result = 4179871 in 2711091 ticks
         // Problem 23, Solution 3: Result = 4179871 in 6501380 ticks
         // Problem 23, Solution 4: Result = 4179871 in 2580855 ticks
+        // Problem 23, Solution 5: Result = 4179871 in 147792 ticks
 
         private static IEnumerable<int> GetDividers(int input)
         {
@@ -213,6 +214,68 @@ namespace EulerProject
                 {
                     total += i;
                 }
+            }
+
+            timer.Stop();
+            return total;
+        }
+
+
+        //////////////////////////////////////////////////////
+
+
+        // Solution heavily inspired from thomad16 on
+        // the Euler Project's forum (https://projecteuler.net/thread=23;page=9)
+        [EulerSolution]
+        public static int Solution5(Stopwatch timer)
+        {
+            timer.Restart();
+
+            var abundant = new bool[28123];
+            for (int i = 12; i < 28123; i++)
+            {
+                if (abundant[i])
+                    continue;
+
+                int facts = 1;
+                int sqrt = (int)Math.Sqrt(i);
+                if (sqrt*sqrt == i)
+                    facts += sqrt;
+                else
+                    sqrt += 1;
+
+                for (int j = 2; j < sqrt; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        facts += j;
+                        facts += (i/j);
+                    }
+                }
+
+                if (facts > i)
+                {
+                    for (int j = i; j < 28123; j+=i)
+                    {
+                        abundant[j] = true;
+                    }
+                }
+            }
+
+            int total = 276; // 1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20+21+22+23;
+            for (int i = 24; i < 28111 /*28123-12*/; i++)
+            {
+                int half = i/2;
+                int add = i;
+                for (int j = 12; j <= half; j++)
+                {
+                    if (abundant[j] && abundant[i - j])
+                    {
+                        add = 0;
+                        break;
+                    }
+                }
+                total += add;
             }
 
             timer.Stop();
