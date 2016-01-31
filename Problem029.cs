ccdf9a781796553edbd270711b8a5ccc20cfa44b
joce <jocelynlegault@gmail.com>
2016-01-30 23:23:33 -0500

    First solution to problem 29
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ Problem029.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem029
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 9183 in 34831 ticks
+
+        private static IEnumerable<BigInteger> GetAllPowers()
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    yield return BigInteger.Pow(i, j);
+                }
+            }
+        }
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            // Brute force
+            timer.Restart();
+            int res = GetAllPowers().Distinct().Count();
+            timer.Stop();
+            return res;
+        }
+    }
+}
