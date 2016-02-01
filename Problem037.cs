345c362be2cb8e8a8f62a3b5ef79f68f71de278b
joce <jocelynlegault@gmail.com>
2016-02-01 13:03:41 -0500

    New, faster, solution to problem 37.
    Signed-off-by: joce <jocelynlegault@gmail.com>

--- Problem037.cs
+++ Problem037.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace EulerProject
 {
@@ -10,7 +11,8 @@ namespace EulerProject
     {
         // The results I got are of the following order of magnitude:
         //
-        // Solution 1: Result = 748317 in 80747 ticks
+        // Solution 1: Result = 748317 in 72234 ticks
+        // Solution 2: Result = 748317 in 47290 ticks
 
         [EulerSolution]
         public static int Solution1(Stopwatch timer)
@@ -48,14 +50,14 @@ namespace EulerProject
                     bool allPrimes = true;
                     for (int j = 1; j < p.Length; j++)
                     {
-                        string s = p.Substring(0, p.Length-j); // Rotate the number
+                        string s = p.Substring(0, p.Length-j); // Trim right
                         if (primes[int.Parse(s)] == 1)
                         {
                             allPrimes = false;
                             break;
                         }
 
-                        string s1 = p.Substring(j, p.Length-j); // Rotate the number
+                        string s1 = p.Substring(j, p.Length-j); // Trim left
                         if (primes[int.Parse(s1)] == 1)
                         {
                             allPrimes = false;
@@ -75,5 +77,88 @@ namespace EulerProject
 
             return res;
         }
+
+
+        //////////////////////////////////////////////////////
+
+
+        [EulerSolution]
+        public static int Solution2(Stopwatch timer)
+        {
+            timer.Restart();
+
+            // Compute primes.
+            const int max = 799999; // We assume all number will be found below 1M, and we know all the 800k and 900k will fail the test.
+            byte[] primes = new byte[max]; // a prime is represented by a 0.
+            primes.Initialize();
+
+            primes[0] = 1;
+            primes[1] = 1;
+
+            int sqrt = (int)Math.Ceiling(Math.Sqrt(max));
+            for (int i = 2; i < sqrt; i++)
+            {
+                if (primes[i] == 0)
+                {
+                    for (int j = 2*i; j < max; j+=i)
+                    {
+                        primes[j] = 1;
+                    }
+                }
+            }
+
+            List<int> l = new List<int>();
+
+            for (int i = 21; i < max; i+=2)
+            {
+                if (primes[i] == 0) // 0 => prime.
+                {
+                    string p = i.ToString();
+                    string firstNum = p[0].ToString();
+                    if (primes[int.Parse(firstNum)] == 1) // == 1 means not a prime
+                    {
+                        // We'll skip the whole group that starts with a non prime because none can match the requirement.
+                        // e.g. skip the 100's, 400's, 8000's, 90000's etc.
+                        var sb = new StringBuilder((int.Parse(firstNum)+1).ToString());
+                        for (int j = 0; j < p.Length - 2; j++)
+                        {
+                            sb.Append("0");
+                        }
+                        sb.Append("1"); // make sure we get to an odd number
+                        i = int.Parse(sb.ToString());
+                        continue;
+                    }
+
+                    bool allPrimes = true;
+                    for (int j = 1; j < p.Length; j++)
+                    {
+                        string s = p.Substring(0, p.Length-j); // Trim right
+                        if (primes[int.Parse(s)] == 1)
+                        {
+                            allPrimes = false;
+                            break;
+                        }
+
+                        s = p.Substring(j, p.Length-j); // Trim left
+                        if (primes[int.Parse(s)] == 1)
+                        {
+                            allPrimes = false;
+                            break;
+                        }
+                    }
+
+                    if (allPrimes)
+                    {
+                        l.Add(i);
+                    }
+                }
+            }
+
+            int res = l.Sum();
+            timer.Stop();
+
+            return res;
+        }
+
     }
 }
