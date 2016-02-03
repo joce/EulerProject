d1b3fefd9e345e4048a0afc83f7349f6312ff987
joce <jocelynlegault@gmail.com>
2016-02-02 23:53:49 -0500

    First solution to problem 63
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ Problem063.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem063
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 49 in 220 ticks
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+
+            int cnt = 0;
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int pow = 1; pow <= 21; pow++)
+                {
+                    if (BigInteger.Pow(i, pow).ToString().Length == pow)
+                    {
+                        cnt++;
+                    }
+                }
+            }
+
+            timer.Stop();
+            return cnt;
+        }
+    }
+}
