c8bfd499da3c09bcf954e99cd6667d5d6a228723
joce <jocelynlegault@gmail.com>
2016-02-07 00:06:16 -0500

    First solution to problem 206
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 201-250/Problem206.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem206
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 1389019170 in 33287345 ticks
+
+        [EulerSolution]
+        public static ulong Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            ulong i =   (ulong)Math.Sqrt(1020304050607080900);
+            ulong max = (ulong)Math.Sqrt(1929394959697989990);
+            for (; i < max; i+=10)
+            {
+                var s = (i*i).ToString();
+                if (s[0] == '1' && s[2] == '2' && s[4] == '3' && s[6] == '4' && s[8] == '5' &&
+                    s[10] == '6' && s[12] == '7' && s[14] == '8' && s[16] == '9' && s[18] == '0')
+                    break;
+            }
+            timer.Stop();
+
+            return i;
+        }
+    }
+}
