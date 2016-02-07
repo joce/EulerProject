c23ad9ef4efaee5ad09f3a4aec10e2f70dbf47f8
joce <jocelynlegault@gmail.com>
2016-02-07 15:42:30 -0500

    First solution to problem 53
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 051-100/Problem053.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem053
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 4075 in 141 ticks
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+
+            double expLog = 0.0;
+            double [] expLogs = new double[101];
+            for (int i = 1; i <= 100; i++)
+            {
+                expLog += Math.Log10(i);
+                expLogs[i] = expLog;
+            }
+
+            int overOneMillion = 0;
+            for (int n = 23; n <= 100; n++)
+            {
+                for (int r = 1; r <= n; r++)
+                {
+                    double combLog = expLogs[n] - (expLogs[r] + expLogs[n - r]);
+                    if (combLog >= 6.0)
+                    {
+                        overOneMillion++;
+                    }
+                }
+            }
+
+            timer.Stop();
+            return overOneMillion;
+        }
+    }
+}
