21685d75abc8c65f5255c173466c92ba9cfe07e5
joce <jocelynlegault@gmail.com>
2016-02-07 20:32:04 -0500

    First "solution" to problem 79
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 051-100/Problem079.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem079
+    {
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            // I did this one by hand.
+            // I sorted the entries, removed the duplicates
+            // and then checked which number came before which other.
+            return 73162890;
+        }
+    }
+}
