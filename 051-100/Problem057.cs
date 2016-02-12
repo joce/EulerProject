da1f0e9220bd5d00ae6ebd0d61a9f2b347485d2b
joce <jocelynlegault@gmail.com>
2016-02-12 18:56:30 -0400

    First solutions to problems 57 and 145
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 051-100/Problem057.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem057
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 153 in 18854 ticks
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            BigInteger num = 3;
+            BigInteger den = 2;
+            int cnt = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((int) BigInteger.Log10(num) > (int) BigInteger.Log10(den))
+                {
+                    cnt++;
+                }
+                BigInteger newNum = 2*den + num;
+                BigInteger newDen = num + den;
+                num = newNum;
+                den = newDen;
+            }
+            timer.Stop();
+            return cnt;
+        }
+    }
+}
---
+++ 101-150/Problem145.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem145
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 608720 in 1070855273 ticks
+        // Solution 2: Result = 608720 in 250003118 ticks
+
+        // Brute force solution
+        [EulerSolution(false, reason: "Takes too long")]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            int cnt = 0;
+            for (long i = 11; i < 1000000000; i+=2)
+            {
+                string s = i.ToString();
+                if (s[s.Length - 1] == '0')
+                {
+                    continue;
+                }
+
+                if ((s[0]-'0' + s[s.Length-1]-'0') % 2 == 0)
+                {
+                    i-=3;
+                    continue;
+                }
+
+                long rev = long.Parse(new string(s.Reverse().ToArray()));
+                rev += i;
+                string s1 = rev.ToString();
+                bool found = true;
+                foreach (char c in s1)
+                {
+                    if (c%2 == 0)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    cnt++;
+                }
+            }
+            timer.Stop();
+            return cnt;
+        }
+
+
+        //////////////////////////////////////////////////////
+
+
+        static int reverse(int i)
+        {
+            int n = i, c = 0;
+
+            int[] digits = new int[10];
+            int nn = 0;
+
+            while (n != 0)
+            {
+                int digit = n % 10;
+                n /= 10;
+                digits[c] = digit;
+
+                nn *= 10;
+                nn += digits[c++];
+            }
+
+            return nn;
+        }
+
+        static bool checkDigits(int n)
+        {
+            int i = n;
+            while (i != 0)
+            {
+                int digit = i % 10;
+                i /= 10;
+
+                if (digit % 2 == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        [EulerSolution]
+        public static int Solution2(Stopwatch timer)
+        {
+            timer.Restart();
+
+            int cnt = 0;
+            for (int i = 1; i < 1000000000; i++)
+            {
+                if (i % 10 != 0)
+                {
+                    cnt += checkDigits(i + reverse(i)) ? 1 : 0;
+                }
+            }
+
+            timer.Stop();
+            return cnt;
+        }
+    }
+}
