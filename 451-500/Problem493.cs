f77b7504858c0e1755ad59c0f03565c31acc0007
joce <jocelynlegault@gmail.com>
2016-02-14 13:53:57 -0400

    First "solution" to problem 493
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 451-500/Problem493.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem493
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = xxx in xxx ticks
+
+        [EulerSolution]
+        public static double Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            // Got this from Wolfram Alpha:
+            //
+            // 7 * (1 - (60 choose 20) / (70 choose 20))
+            timer.Stop();
+            return 6.818741802;
+        }
+    }
+}
