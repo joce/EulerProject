275f5f6fef4d82f967509be07b0c357a11807467
joce <jocelynlegault@gmail.com>
2016-02-14 19:21:58 -0400

    First solution to problem 62
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 051-100/Problem062.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem062
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 127035954683 in 870569 ticks
+
+        static long OrderedPermutation(long val)
+        {
+            char[] orderedChar = val.ToString().OrderBy(c => c != '0' ? c : c+10).ToArray();
+            return long.Parse(new string(orderedChar));
+        }
+
+        [EulerSolution]
+        public static long Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+            var r = Enumerable.Range(1, 100000)
+                              .Select(v => (long) v*v*v)
+                              .GroupBy(OrderedPermutation)
+                              .Where(g => g.Count() == 5)
+                              .SelectMany(g => g.Select(e => e))
+                              .OrderBy(v => v)
+                              .First();
+            timer.Stop();
+            return r;
+        }
+    }
+}
