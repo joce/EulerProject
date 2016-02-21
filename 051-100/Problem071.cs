202b0e11086cc3ff7389b45bf3d3b32e401b5221
joce <jocelynlegault@gmail.com>
2016-02-21 17:18:33 -0500

    First solution to problem 71 and 102
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ 051-100/Problem071.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem071
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 428570 in 8821 ticks
+
+        [EulerSolution]
+        public static long Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+
+            long bestNum = 0;
+            long bestDen = 1;
+
+            for (int den = 1000000; den > 2; den--)
+            {
+                long num = (3*den - 1)/7;
+                if (num*bestDen > bestNum*den)
+                {
+                    bestNum = num;
+                    bestDen = den;
+                }
+            }
+
+            timer.Stop();
+            return bestNum;
+        }
+    }
+}
---
+++ 101-150/Problem102.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace EulerProject
+{
+    [EulerProblem]
+    public class Problem102
+    {
+        // The results I got are of the following order of magnitude:
+        //
+        // Solution 1: Result = 228 in 5931 ticks
+
+        [EulerSolution]
+        public static int Solution1(Stopwatch timer)
+        {
+            timer.Restart();
+
+            // Inspiration drawn from StackOverflow:
+            // http://stackoverflow.com/a/9755252/82682
+            int cnt = 0;
+            foreach (var line in File.ReadAllLines(@"Problem102.data"))
+            {
+                var coords = line.Split(',').Select(int.Parse).ToArray();
+
+                int as_x = -coords[0];
+                int as_y = -coords[1];
+
+                bool s_ab = (coords[2]-coords[0])*as_y-(coords[3]-coords[1])*as_x > 0;
+
+                if ((coords[4]-coords[0])*as_y-(coords[5]-coords[1])*as_x > 0 == s_ab)
+                    continue;
+
+                if ((coords[4]-coords[2])*(-coords[3])-(coords[5]-coords[3])*(-coords[2]) > 0 != s_ab)
+                    continue;
+
+                cnt++;
+            }
+            timer.Stop();
+            return cnt;
+        }
+    }
+}
